fe4d8705845690159378c281a97183383ef05775
David <david.diez31@free.fr>
2017-02-27 23:39:39 +0100

    mise en place sélection couleurs

--- js/index.js
+++ js/index.js
@@ -1,32 +1,61 @@
 $(document).ready(function() {
-    var R, G, B, bgColor;
+    var color1R, color1G, color1B;
+    var color2R, color2G, color2B;
+    var bgColor1 = [];
+    var bgColor2 = [];
 
-    $(function() {
-        var valdef = $('.range').val();
-        $('.range').next().text(valdef); // Valeur par défaut
-        $('.range').on('input', function() {
-            var $set = $(this).val();
-            $(this).next().text($set);
 
-        });
-    });
-
-    $('#r').change(function() {
-        R = $(this).val();
-        console.log("R = " + R);
-    });
-    $('#g').change(function() {
-        G = $('#g').val();
-        console.log("G = " + G);
-    });
-    $('#b').change(function() {
-        B = $('#b').val();
-        console.log("B = " + B);
+    var valdef = $('.range').val();
+    $('.range').next().text(valdef); // Valeur par défaut
+    $('.range').on('input', function() {
+        var $set = $(this).val();
+        $(this).next().text($set);
     });
 
-    $('.range').change('input', function() {
-        bgColor = Number(R + B + G);
-        console.log(bgColor);
+    $('#r1').change(function() {
+        color1R = $(this).val();
+        console.log("R1 = " + color1R);
     });
+
+    $('#r2').change(function() {
+        color2R = $(this).val();
+        console.log("R2 = " + color2R);
+    });
+
+    $('#g1').change(function() {
+        color1G = $(this).val();
+        console.log("G1 = " + color1G);
+    });
+
+    $('#g2').change(function() {
+        color2G = $(this).val();
+        console.log("G2 = " + color2G);
+    });
+
+    $('#b1').change(function() {
+        color1B = $(this).val();
+        console.log("B1 = " + color1B);
+    });
+
+    $('#b2').change(function() {
+        color2B = $(this).val();
+        console.log("B2 = " + color2B);
+    });
+
+    $('.range').change(function() {
+        bgColor1 = [];
+        bgColor2 = [];
+        bgColor1.push(color1R, color1G, color1B);
+        var result1 = bgColor1.toString();
+        bgColor2.push(color2R, color2G, color2B);
+        var result2 = bgColor2.toString();
+        console.log("RGB1= " + result1);
+        console.log("RGB2= " + result2);
+        $('#color1').css('background-color', 'rgb(' + result1 + ')');
+        $('#color2').css('background-color', 'rgb(' + result2 + ')');
+        $('body').css('background-color', 'rgb(' + result1 + ')');
+    });
+
+
 
 });
