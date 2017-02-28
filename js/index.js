fc40aaf6a333b66cc963f99bf8fc9d5cc3b321aa
David <david.diez31@free.fr>
2017-02-28 11:13:57 +0100

    last commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-    var color1R, color1G, color1B;
-    var color2R, color2G, color2B;
-    var bgColor1 = [];
-    var bgColor2 = [];
-
+    var color1R, color1G, color1B, color2R, color2G, color2B, angle;
+    var bgColor1, bgColor2;
 
     var valdef = $('.range').val();
     $('.range').next().text(valdef); // Valeur par défaut
@@ -12,50 +9,25 @@ $(document).ready(function() {
         $(this).next().text($set);
     });
 
-    $('#r1').change(function() {
-        color1R = $(this).val();
-        console.log("R1 = " + color1R);
+    var valdefAngle = $('#angle').val();
+    $('#angle').next().text(valdefAngle); // Valeur par défaut
+    $('#angle').on('input', function() {
+        var $set = $(this).val();
+        $(this).next().text($set);
     });
 
-    $('#r2').change(function() {
-        color2R = $(this).val();
-        console.log("R2 = " + color2R);
+    $('body').on('mousemove', function() {
+        angle = $('#angle').val();
+        color1R = $('#r1').val();
+        color2R = $('#r2').val();
+        color1G = $('#g1').val();
+        color2G = $('#g2').val();
+        color1B = $('#b1').val();
+        color2B = $('#b2').val();
+        bgColor1 = color1R + ',' + color1G + ',' + color1B;
+        bgColor2 = color2R + ',' + color2G + ',' + color2B;
+        $('#color1').css('background-color', 'rgb(' + bgColor1 + ')');
+        $('#color2').css('background-color', 'rgb(' + bgColor2 + ')');
+        $('body').css('background', 'linear-gradient(' + angle + 'deg, rgb(' + bgColor1 + '), rgb(' + bgColor2 + ')');
     });
-
-    $('#g1').change(function() {
-        color1G = $(this).val();
-        console.log("G1 = " + color1G);
-    });
-
-    $('#g2').change(function() {
-        color2G = $(this).val();
-        console.log("G2 = " + color2G);
-    });
-
-    $('#b1').change(function() {
-        color1B = $(this).val();
-        console.log("B1 = " + color1B);
-    });
-
-    $('#b2').change(function() {
-        color2B = $(this).val();
-        console.log("B2 = " + color2B);
-    });
-
-    $('.range').change(function() {
-        bgColor1 = [];
-        bgColor2 = [];
-        bgColor1.push(color1R, color1G, color1B);
-        var result1 = bgColor1.toString();
-        bgColor2.push(color2R, color2G, color2B);
-        var result2 = bgColor2.toString();
-        console.log("RGB1= " + result1);
-        console.log("RGB2= " + result2);
-        $('#color1').css('background-color', 'rgb(' + result1 + ')');
-        $('#color2').css('background-color', 'rgb(' + result2 + ')');
-        $('body').css('background-color', 'rgb(' + result1 + ')');
-    });
-
-
-
 });
